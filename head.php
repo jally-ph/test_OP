29b2d0c4053fbd4238c120faf50eeb83a9d77bcd
jally-ph <phejar.jally@gmail.com>
2019-02-08 19:29:35 +0100

    x

---
+++ head.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cours et Tests</title>
+	<meta charset="utf-8">
+	<!-- Viewport -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+	<!-- Feuilles de styles -->
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+	<!-- Fontawesome -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous"/>
+	<!-- favicon -->
+	<link rel="icon" href="images/favicon.png" />
+	<!--google fonts-->
+	<link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
+</head>
+<body>
+	
+	<!-- banniere -->
+
+	<div class="banniere">
+		<div class="titre_banniere">
+			<div class="logo_banniere">
+				<img src="images/favicon.png">
+			</div>
+			
+			<h1>Cours et Tests</h1>
+			<p>Cours et tests pour Open Classrooms. 2019. JP</p>
+		</div>
+		<img src="images/tulipes.jpeg" alt="banniere" class="image_banniere">
+	</div>
+
+
+</body>
--- index.php
+++ index.php
@@ -1,34 +1,30 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Cours et Tests</title>
-	<meta charset="utf-8">
-	<!-- Viewport -->
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-	<!-- Feuilles de styles -->
-	<link rel="stylesheet" type="text/css" href="style.css">
-	<!-- Bootstrap -->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-	<!-- Fontawesome -->
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous"/>
-	<!-- favicon -->
-	<link rel="icon" href="images/favicon.jpg" />
-</head>
+<?php 
+include('head.php'); 
+?>
+
 <body>
 
-	<h1>Rien que des tests !</h1>
+	<h1>Thèmes</h1>
 
-<h2>Les cours</h2>
+	<div class="blocs">
 
-	<p>
-		<a href=""></a>
-	</p>
+		<div class="bloc_default bloc_php">
+			<p>
+				<a href="">PHP</a>
+			</p>
+		</div>
 
-<h2>Les tests</h2>
+		<div class="bloc_default bloc_bootstrap">
+			<p>
+				<a href="">BOOTSTRAP</a>
+			</p>
+		</div>
 
-	<p>
-		<a href=""></a>
-	</p>
+
+
+	</div>
+	
+
 
 
 
